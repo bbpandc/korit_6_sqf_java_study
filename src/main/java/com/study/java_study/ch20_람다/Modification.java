6e086a32c7c0b53160fcb665b8f57557c284bca0
bbpandc <haeminsia@gmail.com>
2024-06-04 17:15:55 +0900

    빌더, 익명클래스, 람다

---
+++ src/main/java/com/study/java_study/ch20_람다/Modification.java
@@ -0,0 +1,17 @@
+package com.study.java_study.ch20_람다;
+
+/*
+    람다(Lambda)
+    1. 하나의 추상 메소드를 정의하고 있는 인터페이스에서 사용한다.
+    2. 매개변수의 타입을 생략할 수 있다.
+    3. 매개변수가 하나이면 매개변수를 감싸는 () 괄호를 생략할 수 있다.
+    4. 다른 명령 없이 바로 리턴만 한다면 메소드를 정의하는 {} 중괄호와 return 키워드를 생략할 수 있다.
+ */
+
+@FunctionalInterface // 메소드를 하나만 만들 수 있게 해줌(2개 이상 -> 오류)
+// 함수형 인터페이스: 한개의 추상메소드를 가지는 인터페이스
+public interface Modification<T> {
+
+    T modify(T oldData, T newData);
+
+}
